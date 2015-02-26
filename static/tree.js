93f3ed84404db266cf88e793db46f61ced96e4f5
4l1fe <4l1fe@users.noreply.github.com>
2015-02-26 05:58:50 +0300

    поправлен клиент, события, верстка

--- static/tree.js
+++ static/tree.js
@@ -1,55 +1,77 @@
 var render_rows = function(rows, root) {
+    root.children('div').empty();
     $.each(rows, function (k, v) { //'.' в id ломает поиск по селектору
-        var is_ancestor = v['is_ancestor'] ? ' is_ancestor' : '';
-        root.append('<div class="l' + v['lvl'] + is_ancestor + '" id="' + v['code'] + '">' + v['name'] + ' L' + v['lvl'] +
-                    '<button class="edit">edit</button>' +
-                    '<button class="delete">delete</button>' +
-                    '</div>' +
-                    '<br>');
+        var tmpl = v['is_ancestor'] ? '<div class="l' + v['lvl'] + '" id="' + v['code'] + '">' +
+                                      '<span class="is_ancestor">' + v['name'] + ' L' + v['lvl'] + '</span>'
+                                    : '<div class="l' + v['lvl'] + '" id="' + v['code'] + '">' +
+                                      v['name'] + ' L' + v['lvl'];
+        tmpl += '<button class="edit">edit</button>' +
+                '<button class="delete">delete</button>' +
+                '</div>';
+        root.append(tmpl);
     });
-    $('button.edit').on('click', edit);
-    $('button.delete').on('click', del);
 };
 
 var show_descendants = function() {
-    $(this).
+    var span = $(this),
+        div = span.parent(),
+        code = div.attr('id');
+
+    if (code) {
+        $.get('http://127.0.0.1:8888/manage?' + $.param({code: code}))
+            .fail(function (jqxhr, status, error) {
+                alert(error);
+            })
+            .done(function (data, status, jqxhr) {
+                if (data['rows']) {
+                    render_rows(data['rows'], div);
+                };
+            });
+    };
 };
 
 var edit = function() {
     var div = $(this).parent(),
         code = div.attr('id'),
-        name = div.text().split(/ L[0-9]+/)[0],
+        text = div.is(':has(span)') ? div.children('span').text()
+                                    : div.text(),
+        name = text.split(/ L[0-9]+/)[0],
         new_name = prompt('Новое имя:', name);
 
-    $.post('http://127.0.0.1:8888/manage', {code: code, name: name, new_name: new_name})
-        .fail(function (jqxhr, status, error) {
-            alert(error);
-        })
-        .done(function (data, status, jqxhr) {
-            if (data['success']) {
-                div.html(div.html().replace(name, new_name));  // из-за переписывания сносятся обработчики
-                div.children('.edit').on('click', edit);
-                div.children('.delete').on('click', del);
-            }
-        });
+    if (new_name) {
+        $.post('http://127.0.0.1:8888/manage', {code: code, name: name, new_name: new_name})
+            .fail(function (jqxhr, status, error) {
+                alert(error);
+            })
+            .done(function (data, status, jqxhr) {
+                if (data['success']) {
+                    div.html(div.html().replace(name, new_name));
+                }
+            });
+    };
 };
 
 var del = function() {
     var div = $(this).parent(),
         code = div.attr('id');
 
-    $.ajax({type: 'DELETE',
-            url: 'http://127.0.0.1:8888/manage?' + $.param({code: code})})
-    .fail(function(jqxhr, status, error) {
-        alert(error);
-    })
-    .done(function(data, status, jqxhr) {
-        if (data['rows']) {
-            $.each(data['rows'], function(index, value) {
-                $('#'+value).remove();
+    if (code) {
+        $.ajax({
+            type: 'DELETE',
+            url: 'http://127.0.0.1:8888/manage?' + $.param({code: code})
+        })
+            .fail(function (jqxhr, status, error) {
+                alert(error);
+            })
+            .done(function (data, status, jqxhr) {
+                if (data['rows']) {
+                    $.each(data['rows'], function (index, value) {
+                        $('#' + value).remove();
+                    });
+                }
+                ;
             });
-        };
-    });
+    };
 };
 
 var search = function(event) {
@@ -62,7 +84,7 @@ var search = function(event) {
                 })
                 .done(function(data, status, jqxhr) {
                     var root = $('#root');
-                    root.empty();
+                    //root.empty();
                     if (data['rows']) {
                         render_rows(data['rows'], root);
                     };
@@ -72,22 +94,27 @@ var search = function(event) {
 };
 
 var load_data = function() {
-    $.post('http://127.0.0.1:8888/', {lvl: $(this).val()})
-        .fail(function(jqxhr, status, error) {
-            alert(error);
-        })
-        .done(function(data, status, jqxhr) {
-            var root = $('#root');
-            root.empty();
-            if (data['rows']) {
-                render_rows(data['rows'], root);
-            };
-        });
+    var lvl = $(this).val();
+    if (lvl) {
+        $.post('http://127.0.0.1:8888/', {lvl: lvl})
+            .fail(function (jqxhr, status, error) {
+                alert(error);
+            })
+            .done(function (data, status, jqxhr) {
+                var root = $('#root');
+                //root.empty();
+                if (data['rows']) {
+                    render_rows(data['rows'], root);
+                }
+                ;
+            });
+    };
 };
 
 $(function() { // регистрация после рендеринга
-    $('select').on('change', load_data);
-    $('input').on('keypress', search);
-    $('button.edit').on('click', edit);
-    $('button.delete').on('click', del);
+    $(document).on('change', 'select', load_data)
+               .on('keypress', 'input', search)
+               .on('click', '.edit', edit)
+               .on('click', '.delete', del)
+               .on('click', '.is_ancestor', show_descendants);
 });
